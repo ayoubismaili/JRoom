e95ae45f0ba60cb0c8a7392810f595c7c1835d89
Ayoub Ismaili <ayoubismaili1@gmail.com>
2020-05-16 03:35:46 +0100

    Fix License Header

--- JRoomClient/src/com/caporal7/jroom/client/java/dao/ConferenceDao.java
+++ JRoomClient/src/com/caporal7/jroom/client/java/dao/ConferenceDao.java
@@ -1,8 +1,27 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2020 Ayoub Ismaili <ayoubismaili1@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
+
 package com.caporal7.jroom.client.java.dao;
 
 import com.caporal7.jroom.common.java.JRoomSettings;
@@ -14,17 +33,16 @@ import com.caporal7.jroom.common.java.protoc.JRoomProtos.JRoomRequest;
 import com.caporal7.jroom.common.java.protoc.JRoomProtos.JRoomResponse;
 import com.caporal7.jroom.common.java.utils.JRoomUtils;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
-/**
- *
- * @author sphere
- */
-public class JRoomJoinConferenceDao {
+public class ConferenceDao {
     
     private Socket socket;
     
-    public JRoomJoinConferenceDao() throws IOException {
+    public ConferenceDao() throws IOException {
+        // TODO: Change to SSLSocket
         socket = new Socket(JRoomSettings.HOST, JRoomSettings.PORT);
     }
     
@@ -39,22 +57,21 @@ public class JRoomJoinConferenceDao {
                 .build();
         byte[] requestBytes = request.toByteArray();     
         byte[] lengthBytes = JRoomUtils.convertIntToBytes(requestBytes.length);
-        //System.out.println("proto.len = " + requestBytes.length);
-        socket.getOutputStream().write(lengthBytes);
-        socket.getOutputStream().write(requestBytes);
-        socket.getOutputStream().flush();
         
-        lengthBytes = socket.getInputStream().readNBytes(4);
+        OutputStream os = socket.getOutputStream();
+        os.write(lengthBytes);
+        os.write(requestBytes);
+        os.flush();
+        
+        InputStream is = socket.getInputStream();
+        lengthBytes = is.readNBytes(4);
         int length = JRoomUtils.convertBytesToInt(lengthBytes);
-        byte[] responseBytes = socket.getInputStream().readNBytes(length);
+        byte[] responseBytes = is.readNBytes(length);
         
         JRoomResponse response = JRoomResponse.parseFrom(responseBytes);
-        System.out.println(response.getType());
         if (response.getType() != JRoomProtos.Type.JOIN_CONFERENCE_PROB) {
             throw new JRoomException("Invalid response");
         }
         return response.getJoinConferenceProb().getResponse();
     }
-    
-    
 }
